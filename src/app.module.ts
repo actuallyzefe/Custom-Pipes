906ab7c45fe10c3e40b0d04d8e8d7086f4b36983
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-04-06 23:26:41 +0300

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
