9071a6a3b02aee4d47f3577b6eeed60d4b2da246
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-04-07 03:17:05 +0300

    feat: custom pipes

--- src/auth/auth.pipe.ts
+++ src/auth/auth.pipe.ts
@@ -13,10 +13,10 @@ export class loginPipe implements PipeTransform {
   async transform(value: any, metadata: any) {
     const data = plainToInstance(LoginDto, value);
     console.log(data);
-    const tasak = await validate(data, { whitelist: true });
+    const validatedData = await validate(data, { whitelist: true });
     console.log(data);
-    if (tasak.length > 0) throw new Error('ANANIZI SIKEYIM');
-    console.log(tasak);
+    if (validatedData.length > 0) throw new Error('ANANIZI SIKEYIM');
+    console.log(validatedData);
     return data;
   }
 }
